9dd50a6cd5a4edc1e890a17d04df6e67afdb6ae8
juliiiizhu <juliiiizhu@tencent.com>
2019-09-21 20:26:48 +0800

    feat: push trigger ci

--- hello_world/index.js
+++ hello_world/index.js
@@ -1,5 +1,9 @@
 'use strict';
+
+const dayjs = require("dayjs")
+
 exports.main_handler = async (event, context, callback) => {
     console.log("%j", event);
+    console.log("现在时间为：" + dayjs().format('YYYY-MM-DD HH:mm:ss'))
     return "hello world"
 };
