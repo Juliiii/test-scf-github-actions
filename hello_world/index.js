afc8aaa09ac2e0ccdcdf40b11ca258fa86b048b1
Juli <zhulf6@mail2.sysu.edu.cn>
2019-12-12 12:51:13 +0800

    Update index.js

--- hello_world/index.js
+++ hello_world/index.js
@@ -4,6 +4,6 @@ const dayjs = require("dayjs")
 
 exports.main_handler = async (event, context, callback) => {
     console.log("%j", event);
-    console.log("现在时间为：" + dayjs().format('YYYY-MM-DD HH:mm:ss'))
+    console.log("现在时间为12321312：" + dayjs().format('YYYY-MM-DD HH:mm:ss'))
     return "hello world"
 };
